bada0053d5a4e5834a78209c39fdc78db2af5d09
manku <mayanksingh19524@gmail.com>
2026-08-14 21:35:53 +0530

    Deleting all the files from the repository

--- app/src/main/java/com/emergency/mesh/models/UserRole.kt
+++
@@ -1,9 +0,0 @@
-package com.emergency.mesh.models
-
-/**
- * User role in the emergency mesh system
- */
-enum class UserRole {
-    CITIZEN,    // Can send SOS, view messages
-    OFFICIAL    // Can view SOS map and respond
-}
